64e6003704c5e6db01968d1fd1e8506311ab8b6f
Nick Daly <128556497+ndaly94@users.noreply.github.com>
2023-10-05 17:17:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('./config/database'); // runs the file because it gets compiled as an IIFE
 
 const app = require('./app-server');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8097;
 
 app.listen(PORT, () => {
 	console.log(`Fox, the hatch at port ${PORT} is open!`);
